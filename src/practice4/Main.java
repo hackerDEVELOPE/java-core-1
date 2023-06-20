c3ce6ddf58446f09a5e48bb4347a5a8d03892283
Neltharion Earth-Warden <loliikonn@mail.ru>
2023-06-20 23:15:48 +0500

    task was solved/perfect

---
+++ src/practice4/Main.java
@@ -0,0 +1,16 @@
+package practice4;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(squareSum(new int[]{1, 2, 3, 4, 5, 6, 7}));
+    }
+
+    public static int squareSum(int[] n) {
+        int sum = 0;
+        for (int i = 0; i < n.length; i++) {
+            sum += n[i] * n[i];
+
+        }
+        return sum;
+    }
+}
